00f84ea17cf33d25965da4f051779413b976f1c3
Leonardo Henrique <leonardo.hdsrodrigues@gmail.com>
2022-10-27 17:52:55 -0300

    tests: add cypress config

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    baseUrl: 'http://localhost:3000',
+    experimentalSessionAndOrigin: true
+  },
+});
